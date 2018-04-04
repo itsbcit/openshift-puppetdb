aa9c3ecdccc4951e7df0251edd146e0fe3372cf3
Jesse Weisner <jesse_weisner@bcit.ca>
2018-04-04 10:31:13 -0700

    Fixed multiline conditional

--- 70-ssl-setup.sh
+++ 70-ssl-setup.sh
@@ -1,8 +1,7 @@
-if [ \
-        -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/ca.pem \
-     && -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/private_keys/puppetdb.pem \
-     && -f opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/puppetdb.pem \
-   ];then
+if [ -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/ca.pem ] && \
+   [ -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/private_keys/puppetdb.pem ] && \
+   [ -f opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/puppetdb.pem ]
+then
     /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
 else
     echo "\t *** SSL Certificates missing! ***"
