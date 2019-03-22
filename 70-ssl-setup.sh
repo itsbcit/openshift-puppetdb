98f9b465f600f956064f3d1233ea1652cad2eee5
Jesse Weisner <jesse_weisner@bcit.ca>
2019-03-21 19:51:37 -0700

    rewritten

--- 70-ssl-setup.sh
+++ 70-ssl-setup.sh
@@ -1,8 +1,16 @@
-if [ -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/ca.pem ] && \
-   [ -f /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/private_keys/puppetdb.pem ] && \
-   [ -f opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet/ssl/certs/puppetdb.pem ]
-then
-    /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
+if [ -r /etc/puppetlabs/puppetdb/ssl/ca.pem ] && \
+   [ -r /etc/puppetlabs/puppetdb/ssl/private.pem ] && \
+   [ -r /etc/puppetlabs/puppetdb/ssl/public.pem ]; then
+    echo "    *** Found existing SSL files ***"
 else
-    echo "    *** SSL Certificates missing! ***"
+    echo "    *** PuppetDB SSL files not found ***"
+
+    if [ -r /etc/puppetlabs/puppet/ssl/certs/ca.pem ] && \
+       [ -r /etc/puppetlabs/puppet/ssl/signed/${PUPPET_CERTNAME}.pem ] && \
+       [ -r /etc/puppetlabs/puppet/ssl/private_keys/${PUPPET_CERTNAME}.pem ];then
+        echo "    *** Copying SSL files from puppetserver ***"
+    else
+        echo "    *** Attempting PuppetDB ssl-setup ***"
+        /opt/puppetlabs/server/bin/puppetdb ssl-setup
+    fi
 fi
